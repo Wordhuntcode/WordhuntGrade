f3fa1cd4d965d641d64c8d77c8db6551c8934815
Wordhuntcode <tylerurbach143@icloud.com>
2025-03-29 07:36:12 -0700

    Update server.py

--- server.py
+++ server.py
@@ -27,5 +27,8 @@ def score_grid():
         "words": scored_words
     })
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
